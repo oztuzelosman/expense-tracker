471f20e1a6d748d8de86e913b7b8a1cc6b2bff09
Osman Oztuzel <oztuzelosman@gmail.com>
2022-03-30 10:38:09 +0300

    finish prop

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,16 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2022, 3, 18);
+function ExpenseItem(props) {
+ /*  const expenseDate = new Date(2022, 3, 18);
   const expenseTitle = "Buying a car";
-  const expenseAmount = 7000;
+  const expenseAmount = 7000; */
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
