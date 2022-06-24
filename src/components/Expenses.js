e925c01903ccd8da579a88b04ede3622456ffa1d
oztuzelosman <oztuzelosman@gmail.com>
2022-06-24 13:41:11 +0300

    create Expenses.js and move ExpenseItems into it

---
+++ src/components/Expenses.js
@@ -0,0 +1,44 @@
+import ExpenseItem from "./ExpenseItem";
+
+function Expenses() {
+
+    const expenses = [
+        { title: "Buying Car", amount: 7000, date: new Date(2022, 3, 18) },
+        { title: "Grocery Shopping", amount: 1000, date: new Date(2021, 9, 28) },
+        { title: "Pharmacy", amount: 10, date: new Date(2022, 3, 8) },
+        { title: "Book Shopping", amount: 30, date: new Date(2022, 1, 13) },
+        { title: "Travelling Fees", amount: 100, date: new Date(2021, 2, 3) }
+      ];
+
+  return (
+    <div>
+    <ExpenseItem
+      title={expenses[0].title}
+      amount={expenses[0].amount}
+      date={expenses[0].date}
+    ></ExpenseItem>
+    <ExpenseItem
+      title={expenses[1].title}
+      amount={expenses[1].amount}
+      date={expenses[1].date}
+    ></ExpenseItem>
+    <ExpenseItem
+      title={expenses[2].title}
+      amount={expenses[2].amount}
+      date={expenses[2].date}
+    ></ExpenseItem>
+    <ExpenseItem
+      title={expenses[3].title}
+      amount={expenses[3].amount}
+      date={expenses[3].date}
+    ></ExpenseItem>
+     <ExpenseItem
+      title={expenses[4].title}
+      amount={expenses[4].amount}
+      date={expenses[4].date}
+    ></ExpenseItem>
+  </div>
+  );
+}
+
+export default Expenses;
