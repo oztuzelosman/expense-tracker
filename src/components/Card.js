420cdfe63c9f147bf562ea725240748e0bcff859
oztuzelosman <oztuzelosman@gmail.com>
2022-06-25 16:10:44 +0300

    bugfix with return issue

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,7 @@ import "./Card.css"
 
 function Card(props) {
     const classes = 'card ' + props.className
-  return 
-  <div className={classes}>{props.children}</div>;
+  return <div className={classes}>{props.children}</div>;
 }
 
 export default Card;
