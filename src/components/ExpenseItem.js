e0215cb0fa201bde33a4a7f893925513bd12bf33
oztuzelosman <oztuzelosman@gmail.com>
2022-06-26 15:23:28 +0300

    creating a valuable which works in event function

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,20 @@
-
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "./Card";
 
 function ExpenseItem(props) {
-const clickHandler = () =>{ console.log("FUCK!!")}
+  let initialTitle = props.title;
+
+  const clickHandler = () => {
+    initialTitle = "UPDATED!!";
+    console.log(initialTitle)
+  };
 
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{initialTitle}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
 
