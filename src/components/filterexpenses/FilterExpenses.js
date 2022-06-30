c32d2b83640dbb975da9266d544d5e1834effd29
oztuzelosman <oztuzelosman@gmail.com>
2022-06-30 23:11:08 +0300

    listening changes on select by changeFilterValues function

--- src/components/filterexpenses/FilterExpenses.js
+++ src/components/filterexpenses/FilterExpenses.js
@@ -1,23 +1,23 @@
 import "./FilterExpenses.css";
 
 const FilterExpenses = () => {
+  const changeFilterValue = (event) => {
+    console.log(event.target.value);
+  };
 
-
-
-
-    return (
-        <div className="expenses-filter">
-            <div className="expenses-filter__control">
-                <label>Filter by year</label>
-                <select >
-                    <option value="2022">2022</option>
-                    <option value="2021">2021</option>
-                    <option value="2020">2020</option>
-                    <option value="2019">2019</option>
-                </select>
-            </div>
-        </div>
-    )
+  return (
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
+        <label>Filter by year</label>
+        <select onChange={changeFilterValue}>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+        </select>
+      </div>
+    </div>
+  );
 };
 
 export default FilterExpenses;
