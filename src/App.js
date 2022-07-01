f42007e3d546402a1e9f978d2910ce70f6df4773
oztuzelosman <oztuzelosman@gmail.com>
2022-07-01 23:17:29 +0300

    add ids to expenseitems for key value to differ each item

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import Expenses from "./components/Expenses";
 import NewExpense from "./components/newexpenses/NewExpense";
 
 const Dummy_Expenses = [
-  { title: "Buying Car", amount: 7000, date: new Date(2022, 3, 18) },
-  { title: "Grocery Shopping", amount: 1000, date: new Date(2021, 9, 28) },
-  { title: "Pharmacy", amount: 10, date: new Date(2022, 3, 8) },
-  { title: "Book Shopping", amount: 30, date: new Date(2022, 1, 13) },
-  { title: "Travelling Fees", amount: 100, date: new Date(2021, 2, 3) },
+  { id: "01", title: "Buying Car", amount: 7000, date: new Date(2022, 3, 18) },
+  { id: "02", title: "Grocery Shopping", amount: 1000, date: new Date(2021, 9, 28) },
+  { id: "03", title: "Pharmacy", amount: 10, date: new Date(2022, 3, 8) },
+  { id: "04", title: "Book Shopping", amount: 30, date: new Date(2022, 1, 13) },
+  { id: "05", title: "Travelling Fees", amount: 100, date: new Date(2021, 2, 3) },
 ];
 
 function App() {
