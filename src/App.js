3f97c8321b2ff2fa69e3745de0877fb47ac73826
oztuzelosman <oztuzelosman@gmail.com>
2022-07-01 14:47:12 +0300

    add useState to app.js to reflect changes from user will and remove codiing for dummy gross button so that code works for title

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
+import React, { useState } from "react";
 import Expenses from "./components/Expenses";
 import NewExpense from "./components/newexpenses/NewExpense";
 
+const Dummy_Expenses = [
+  { title: "Buying Car", amount: 7000, date: new Date(2022, 3, 18) },
+  { title: "Grocery Shopping", amount: 1000, date: new Date(2021, 9, 28) },
+  { title: "Pharmacy", amount: 10, date: new Date(2022, 3, 8) },
+  { title: "Book Shopping", amount: 30, date: new Date(2022, 1, 13) },
+  { title: "Travelling Fees", amount: 100, date: new Date(2021, 2, 3) },
+];
 
 function App() {
-  const expenses = [
-    { title: "Buying Car", amount: 7000, date: new Date(2022, 3, 18) },
-    { title: "Grocery Shopping", amount: 1000, date: new Date(2021, 9, 28) },
-    { title: "Pharmacy", amount: 10, date: new Date(2022, 3, 8) },
-    { title: "Book Shopping", amount: 30, date: new Date(2022, 1, 13) },
-    { title: "Travelling Fees", amount: 100, date: new Date(2021, 2, 3) }
-  ];
+  const [expenses, setExpenses] = useState(Dummy_Expenses);
 
-  const addExpenseHandler = (items) => {
-console.log(items)
-  }
+  const addExpenseHandler = (expense) => {
+    setExpenses([expense, ...expenses])
+  };
 
   return (
     <div>
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,26 +1,15 @@
-import React, { useState } from "react";
-
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "./Card";
 
 function ExpenseItem(props) {
-  const [initialTitle, setinitialTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setinitialTitle("Updated!")
-    console.log(initialTitle);
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{initialTitle}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-
-     {/*  <button onClick={clickHandler}> Click Me!</button> */}
     </Card>
   );
 }
