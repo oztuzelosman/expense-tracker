bc7df35af30b5c28b3a88545c22e874510cd51a2
oztuzelosman <oztuzelosman@gmail.com>
2022-07-05 12:36:06 +0300

    use logical and operator with components together to show or unshow elements. useState is for saving data to false or true with click handler.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
 import React, { useState } from "react";
 import Expenses from "./components/Expenses";
 import NewExpense from "./components/newexpenses/NewExpense";
-import ButtonAddExpense from "./components/ButtonAddExpense";
+import "./components/ButtonAddExpense.css";
 
 const Dummy_Expenses = [
   { id: "01", title: "Buying Car", amount: 7000, date: new Date(2022, 3, 18) },
-  { id: "02", title: "Grocery Shopping", amount: 1000, date: new Date(2021, 9, 28) },
+  {
+    id: "02",
+    title: "Grocery Shopping",
+    amount: 1000,
+    date: new Date(2021, 9, 28),
+  },
   { id: "03", title: "Pharmacy", amount: 10, date: new Date(2022, 3, 8) },
   { id: "04", title: "Book Shopping", amount: 30, date: new Date(2022, 1, 13) },
-  { id: "05", title: "Travelling Fees", amount: 100, date: new Date(2021, 2, 3) },
+  {
+    id: "05",
+    title: "Travelling Fees",
+    amount: 100,
+    date: new Date(2021, 2, 3),
+  },
 ];
 
 function App() {
   const [expenses, setExpenses] = useState(Dummy_Expenses);
 
   const addExpenseHandler = (expense) => {
-    setExpenses([expense, ...expenses])
+    setExpenses([expense, ...expenses]);
+  };
+
+  const [isShown, setIsShown] = useState(false);
+
+  const clickHandler = () => {
+    setIsShown(true);
+  };
+
+  const cancelHandler = () => {
+    setIsShown(false);
   };
 
   return (
     <div>
-      <ButtonAddExpense></ButtonAddExpense>
-      <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
-      <Expenses items={expenses}></Expenses>
+      <div className="add-expense">
+        <button onClick={clickHandler}>Add New Expense</button>
+      </div>
+      {isShown && (
+        <div>
+          <NewExpense onAddExpense={addExpenseHandler} />
+          <div className="add-expense">
+            <button onClick={cancelHandler}>Cancel</button>
+          </div>
+        </div>
+      )}
+      <Expenses items={expenses} />
     </div>
   );
 }
--- src/components/newexpenses/ExpenseForm.js
+++ src/components/newexpenses/ExpenseForm.js
@@ -70,7 +70,6 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
-        <button>Cancel</button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
